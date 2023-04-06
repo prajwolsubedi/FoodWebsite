b2b53db59080b5693baea62429140c5e1efc6e9d
prajwolsubedi <prajwolsubedizzz@gmail.com>
2023-04-06 17:58:08 +0545

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,106 @@
+import { restaurantList } from "../constants.js";
+import RestaurantCard from "./RestaurantCard.js";
+import { useState, useEffect } from "react";
+import ShimmerUI from "./ShimmerUI.js";
+
+const filterData = (searchText, allRestaurant) => {
+  return allRestaurant.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+};
+
+const Body = () => {
+  const [searchText, setSearchText] = useState("");
+  const [allRestaurant, setAllRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  //empty dependency array [] - onetime after the initial render.
+  // dependecy array[restaurant] = onetime after the initial reander and everytime the restaurant changes.
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.2984413999999981&lng=77.99313599999999&page_type=DESKTOP_WEB_LISTING#"
+    );
+    const json = await data.json();
+    //Yesto belama you have to do optional chaining because if there is not data it will break.
+    setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  return allRestaurant.length === 0 || filteredRestaurant.length === 0 ? (
+    <ShimmerUI />
+  ) : (
+    <>
+      <div className="search-bar">
+        <h1>Search Bar</h1>
+        <input
+          placeholder="Search"
+          value={searchText}
+          type="text"
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          type="submit"
+          onClick={() => {
+            setFilteredRestaurant(filterData(searchText, allRestaurant));
+          }}
+        >
+          Submit
+        </button>
+      </div>
+      <div className="card-container">
+        {filteredRestaurant.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
+import ShimmerUI from "./ShimmerUI";
 
 const filterData = function (inputText, restaurant) {
   return restaurant.filter((restaurant) =>
@@ -10,7 +110,8 @@ const filterData = function (inputText, restaurant) {
 
 const Body = () => {
   const [inputText, setInputText] = useState("");
-  const [restaurant, setRestaurant] = useState(restaurantList);
+  const [allRestaurant, setAllRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
   //empty dependency array [] - onetime after the initial render.
   // dependecy array[restaurant] = onetime after the initial reander and everytime the restaurant changes.
@@ -28,10 +129,15 @@ const Body = () => {
     );
     const json = await data.json();
     console.log(json);
-    setRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
   }
 
-  return (
+  // if(!allRestaurant) return null;
+
+  return allRestaurant.length === 0 ? (
+    <ShimmerUI />
+  ) : (
     <>
       <div className="search-bar-container">
         <input
@@ -47,7 +153,7 @@ const Body = () => {
             // if (inputText === "") {
             //   setRestaurant(restaurantList);
             // } else {
-            setRestaurant(filterData(inputText, restaurant));
+              setFilteredRestaurant(filterData(inputText, allRestaurant));
             // }
           }}
         >
@@ -55,7 +161,7 @@ const Body = () => {
         </button>
       </div>
       <div className="card-container">
-        {restaurant.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
         ))}
       </div>
@@ -64,3 +170,6 @@ const Body = () => {
 };
 
 export default Body;
+
+
+*/
