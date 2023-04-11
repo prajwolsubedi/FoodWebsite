3cc6887db3e0bef9915ba0d8b7173b04ca1df6b0
prajwolsubedi <prajwolsubedizzz@gmail.com>
2023-04-11 06:11:11 +0545

    update

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,25 +1,16 @@
-import { IMG_CDN_URL } from "../constants";
+import {IMG_CDN_URL} from "../constants"
 
-const RestaurantCard = ({
-  cloudinaryImageId,
-  name,
-  cuisines,
-  address,
-  avgRating,
-}) => {
-  return (
-    <div className="card">
-      <img
-        className="card_img"
-        alt="image"
-        src={IMG_CDN_URL + cloudinaryImageId}
-      />
-      <h2>{name}</h2>
-      <h3>{cuisines.join(", ")}</h3>
-      <h4>{address}</h4>
-      <h5>{avgRating} ⭐️ rating</h5>
-    </div>
-  );
-};
+const RestaurantCard = ({cloudinaryImageId,name,avgRating,cuisines}) => {
+  return(
+    <>
+      <div className="card">
+          <img className="card-img" alt="image" src={IMG_CDN_URL + cloudinaryImageId} />
+          <h2>{name}</h2>
+          <h3>{cuisines.join(", ")}</h3>
+          <h4> {avgRating} ⭐️ Ratings</h4>
+      </div>
+    </>
+  )
+}
 
 export default RestaurantCard;
