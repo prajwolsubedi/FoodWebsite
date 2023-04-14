6afca28f0659c8487b6258536f69dc5eecb67cf2
prajwolsubedi <prajwolsubedizzz@gmail.com>
2023-04-14 15:06:04 +0545

    update

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+
+
+const About = () => {
+    return(
+        <>
+            <div>
+                <h1> Best Restaurant in the Town.  You may want to indulge 🍟🍔</h1>
+            </div>
+        </>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+const Contact = () => {
+  return (
+    <div>
+        <h1>Contact me!!</h1>
+        <h2>Nyaye napaye Gorkha janu Website banaunu parye Prajwol lai samjanu.</h2>
+        {/* <p>Nyaye napaye Gorkha janu. Website banaunu parye prajwol lai samjanu 🤙</p> */}
+    </div>
+  )
+}
+
+export default Contact
