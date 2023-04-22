31243cd7757692bb79fa38a8ce59158ae32dcf4c
prajwolsubedi <prajwolsubedizzz@gmail.com>
2023-04-22 21:57:32 +0545

    update

--- utils/useOnline.js
+++ utils/useOnline.js
@@ -1,14 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const useOnline = () => {
   const [isOnline, setIsOnline] = useState(true);
-  const handleOnline = () => setIsOnline(true);
-  const handleOffline = () => setIsOnline(false);
   useEffect(() => {
     window.addEventListener("online", () => {
-      handleOnline();
+      handleOnline = () => setIsOnline(true);
     });
     window.addEventListener("offline", () => {
-      handleOffline();
+      handleOffline = () => setIsOnline(false);
     });
     return () => {
       window.removeEventListener("online", handleOnline);
