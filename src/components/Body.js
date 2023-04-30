2ca2d3decf3733eecd6ddac6a3fd0137cde4626d
prajwolsubedi <prajwolsubedizzz@gmail.com>
2023-04-30 09:30:24 +0545

    update

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,45 +1,32 @@
 import RestaurantCard from "./RestaurantCard.js";
-import { useState, useEffect } from "react";
+import { useState, useContext } from "react";
 import ShimmerUI from "./ShimmerUI.js";
-
-const filterData = (searchText, allRestaurant) => {
-  return allRestaurant.filter((restaurant) =>
-    restaurant?.data?.name?.toLowerCase()?.includes(searchText.toLowerCase())
-  );
-};
-
+import { Link } from "react-router-dom";
+import { filterRestaurant } from "../../utils/helper.js";
+import useOnline from "../../utils/useOnline.js";
+import useRestaurant from "../../utils/useRestaurant.js";
+import UserContext from "../../utils/UserContext.js";
 const Body = () => {
+  const { newUser, setNewUser } = useContext(UserContext);
+  console.log(newUser, setNewUser);
   const [searchText, setSearchText] = useState("");
-  const [allRestaurant, setAllRestaurant] = useState([]);
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const allRestaurant = useRestaurant(setFilteredRestaurant);
+  const isOnline = useOnline();
 
-  useEffect(() => {
-    getRestaurants();
-  }, []);
-
-  async function getRestaurants() {
-    try {
-      const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.2984413999999981&lng=77.99313599999999&page_type=DESKTOP_WEB_LISTING#"
-      );
-      const json = await data.json();
-      setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
-      setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
-    } catch (error) {
-      console.log(error, "mero ho hai");
-      setAllRestaurant([]);
-      setFilteredRestaurant([]);
-    }
-  }
-
+  if (!isOnline)
+    return (
+      <h1 className="m-2 p-2 text-bold text-8xl">Oops you are offline!</h1>
+    );
   //undefined nai set hudo raixa value xaina vane
   return !allRestaurant || allRestaurant.length === 0 ? (
     <ShimmerUI />
   ) : (
     <>
-      <div className="search-bar">
+      <div className="p-5 bg-pink-50">
         <h1>Search Bar</h1>
         <input
+          className="border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 p-2 m-2 focus:bg-green-50"
           placeholder="Search"
           value={searchText}
           type="text"
@@ -48,21 +35,49 @@ const Body = () => {
           }}
         />
         <button
+          className="m-2 p-1 bg-purple-600 text-white rounded-lg hover:bg-purple-900 text-sm"
           type="submit"
           onClick={() => {
-            setFilteredRestaurant(filterData(searchText, allRestaurant));
+            setFilteredRestaurant(filterRestaurant(searchText, allRestaurant));
           }}
         >
           Submit
         </button>
+        <input
+          value={newUser.name}
+          onChange={(e) =>
+            setNewUser({
+              newUser: {
+                ...newUser,
+                name: e.target.value,
+              },
+            })
+          }
+        />
+        <input
+          value={newUser.email}
+          onChange={(e) =>
+            setNewUser({
+              newUser: {
+                ...newUser,
+                email: e.target.value,
+              },
+            })
+          }
+        />
       </div>
 
-      <div className="card-container">
+      <div className="m-5 flex flex-wrap justify-between shadow-100">
         {filteredRestaurant?.length === 0 ? (
           <h1>Item not found</h1>
         ) : filteredRestaurant ? (
-          filteredRestaurant.map((restaurant) => (
-            <RestaurantCard {...restaurant.data} key={restaurant?.data?.id} />
+          filteredRestaurant?.map((restaurant) => (
+            <Link
+              to={"/restaurant/" + restaurant.data.id}
+              key={restaurant?.data?.id}
+            >
+              <RestaurantCard {...restaurant.data} />
+            </Link>
           ))
         ) : (
           <h1>Loading...</h1>
