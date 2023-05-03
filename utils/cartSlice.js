4db64bb8b4d5a9a7b0d8550e684f904b8c7dd1dd
prajwolsubedi <prajwolsubedizzz@gmail.com>
2023-05-03 07:41:40 +0545

    cartSlice.js

---
+++ utils/cartSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    //Why we can't do = here
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    //   console.log(action);
+    },
+    removeItem: (state) => {
+      state.items.pop();
+    },
+    clearCart: (state) => {
+      state.items = [];
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
